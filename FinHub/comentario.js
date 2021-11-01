701f85ab80d88373f9db1bfac2ee85a908fb0d1d
Gabriel <gabriel-ber@outlook.com>
2021-10-31 23:10:16 -0300

    insere comentario

---
+++ FinHub/comentario.js
@@ -0,0 +1,24 @@
+function publicaComentario() {
+    var formName = document.getElementById("formName");
+    var formComent = document.getElementById("formComent");
+
+    var dados = JSON.parse(localStorage.getItem("dadosComent"));
+
+    if (dados == null) {
+        localStorage.setItem("dadosComent", "[]");
+        dados = [];
+    } 
+    var auxRegistro = {
+        nome: formName.value,
+        comentario: formComent.value
+    }
+    dados.push(auxRegistro);
+
+    localStorage.setItem("dadosComent", JSON.stringify(dados));
+
+    alert("Comentário publicado")
+
+    formName.value = "";
+    formComent.value = "";
+
+}
