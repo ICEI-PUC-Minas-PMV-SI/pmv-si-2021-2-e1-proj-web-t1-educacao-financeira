669e31a82166a488fd0f932a87cadd62c7f0e14c
Tulio Ribeiro Maia <89429230+TulioRibeiroMaia@users.noreply.github.com>
2021-11-02 12:11:10 -0300

    Pag apps(Add like,comment and description).

---
+++ FinHub/script.js
@@ -0,0 +1,32 @@
+function leiaMais(){
+    var pontos=document.getElementById("pontos");
+    var maisTexto=document.getElementById("mais");
+    var btnLeiaMais=document.getElementById("btnLeiaMais");
+
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maisTexto.style.display="none";
+        btnLeiaMais.innerHTML="LeiaMais";
+    }else{
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+        btnLeiaMais.innerHTML="Leia Menos";
+    }
+   }
+   
+   function leiaMais2(){
+    var pontos=document.getElementById("pontos2");
+    var maisTexto=document.getElementById("mais2");
+    var btnLeiaMais=document.getElementById("btnLeiaMais2");
+
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maisTexto.style.display="none";
+        btnLeiaMais.innerHTML="LeiaMais";
+    }else{
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+        btnLeiaMais.innerHTML="Leia Menos";
+    }
+   }
+   
