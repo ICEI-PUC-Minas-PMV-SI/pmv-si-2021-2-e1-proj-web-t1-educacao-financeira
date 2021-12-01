ddda6e54102f4bd22ee3a1fbac6daf2c963c6312
Mikaele Costa <mikaele.costa@sga.pucminas.br>
2021-11-30 21:50:09 -0300

    exibe comentários no html

--- src/js/comentario.js
+++ src/js/comentario.js
@@ -22,3 +22,25 @@ function publicaComentario() {
     formComent.value = "";
 
 }
+function main() {
+    var dados = JSON.parse(localStorage.getItem("dadosComent"));
+    // console.log(dados)
+    let a = retornaDadoHTLM(dados);
+}
+function retornaDadoHTLM(dados){
+
+    let array = Object.values(dados); //transforma objeto em array
+
+    for(i=0; i<array.length; i++){
+        let node_name = document.createElement("h6"); //cria elemento
+        let name = document.createTextNode(array[i].nome); //cria texto com os dados do array na posição declarada
+        node_name.appendChild(name); //adiciona o texto ao elemento 
+
+        let node_comentario = document.createElement("p");
+        let comentario = document.createTextNode(array[i].comentario);
+        node_comentario.appendChild(comentario);
+
+        document.getElementById("comentarios").appendChild(node_name); //adiciona no html o elemento criado com os dados
+        document.getElementById("comentarios").appendChild(node_comentario);}  
+}
+main()
